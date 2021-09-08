137254a4903da716b068885bf23d1b0c10a94e53
Yannick Malins <yannick.malins@symphony.com>
2021-09-08 17:08:52 +0200

    add code + swagger + postman

---
+++ src/main/java/com/yannick/SimpleChatService/ChatSecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.yannick.SimpleChatService;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class ChatSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("a").password(encoder.encode("password")).roles("USER")
+                .and()
+                .withUser("b").password(encoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder.encode("password")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic().and().cors().and().csrf().disable();
+    }
+}
